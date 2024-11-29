6a9dfa0170c7f384f892920fd136dfcb7bb884ee
freshartclub <devops@freshartclub.com>
2024-11-29 21:53:53 +0530

    added baseURL

--- src/utils/axiosUtils.jsx
+++ src/utils/axiosUtils.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:4000",
-  // baseURL: "https://voosh-server-32um.onrender.com",
+  // baseURL: "http://localhost:4000",
+  baseURL: "https://vrv-backend-eys6.onrender.com",
 });
 
 export default axiosInstance;
